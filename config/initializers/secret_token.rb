21fc4b966dc915aaa1e6c94671cc633b793599cf
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2011-12-13 14:47:46 -0500

    [https://www.pivotaltracker.com/story/show/22248215] Add default Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalesVerge::Application.config.secret_token = '6a2d794977d7867c7b7e78f19f100fa3b6df4faf5d6dd116402444565d0f8fa1e4794eeecb67d86371f1fa5dbe669b97f311c85907ade36dd5cbcef4f79dc494'
